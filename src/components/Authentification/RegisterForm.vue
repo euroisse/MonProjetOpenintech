85931108e36693ce4b2d4eb9ed4d553b5b7ea525
euroisse <euroissetsopkeng@gmail.com>
2025-08-19 19:11:00 +0100

    Refactor Login and RegisterForm components for improved layout and styling; enhance input field accessibility and error handling

--- src/components/Authentification/RegisterForm.vue
+++ src/components/Authentification/RegisterForm.vue
@@ -1,8 +1,5 @@
 <template>
- 
-  <div
-    class=" pt-10 flex w-full max-w-full mx-auto items-center justify-center"
-  >
+  <div class="pt-10 flex w-full max-w-full mx-auto items-center justify-center">
     <div
       class="bg-[#F9FAFB] rounded-lg shadow-lg md:w-full overflow-hidden max-w-4xl flex"
     >
@@ -13,12 +10,16 @@
           class="h-full w-full object-cover"
           alt="Voiture de luxe"
         />
-        <div class="absolute" style="z-index: 99; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-          <img
-            src="../Images/logo.png"
-            alt="logo-image"
-            class=" left-4 w-16"
-          />
+        <div
+          class="absolute"
+          style="
+            z-index: 99;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+          "
+        >
+          <img src="../Images/logo.png" alt="logo-image" class="left-4 w-16" />
         </div>
       </div>
       <div class="w-full md:w-1/2 p-8">
@@ -39,7 +40,7 @@
                 <input
                   type="text"
                   v-model="utilisateur.firstname"
-                  class="w-full px-4 py-2 border text-center rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                  class="w-full pl-10 py-2 border text-left rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
                   required
                   placeholder="Entrez votre nom"
                 />
@@ -61,7 +62,7 @@
                 <input
                   type="text"
                   v-model="utilisateur.lastname"
-                  class="w-full px-4 py-2 text-center border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                  class="w-full pl-10 py-2 text-left border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
                   placeholder="Entrez votre prénom"
                 />
               </div>
@@ -69,7 +70,7 @@
             <!-- Champ Email -->
             <div class="mb-4">
               <div class="flex flex-col gap-1">
-                <label class="block text-black "
+                <label class="block text-black"
                   >Email <span class="text-red-500"> *</span></label
                 >
                 <div class="relative">
@@ -79,7 +80,7 @@
                   <input
                     type="email"
                     v-model="utilisateur.email"
-                    class="w-full px-4 py-2 text-center border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                    class="w-full pl-10 py-2 text-left border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
                     required
                     placeholder="Entrez votre Email"
                   />
@@ -102,7 +103,7 @@
                   <input
                     type="password"
                     v-model="utilisateur.password"
-                    class="w-full px-4 py-2 text-center border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                    class="w-full pl-10 py-2 text-left border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
                     required
                     placeholder="Entrez votre mot de passe"
                   />
@@ -124,11 +125,10 @@
                   <input
                     type="password"
                     v-model="utilisateur.password_confirmation"
-                    class="w-full px-4 py-2 text-center border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                    class="w-full pl-10 py-2 text-left border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
                     required
                     placeholder="Entrez votre mot de passe"
                   />
-                
                 </div>
               </div>
             </div>
@@ -144,7 +144,7 @@
                   <input
                     type="tel"
                     v-model="utilisateur.phone"
-                    class="w-full px-4 py-2 border text-center rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                    class="w-full pl-10 py-2 border text-left rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
                     placeholder="Entrez votre numéro"
                   />
                 </div>
@@ -162,9 +162,13 @@
               <div class="flex flex-col gap-1">
                 <div class="relative">
                   <span class="icon-style"
-                    ><i class="ri-login-box-line text-black cursor-pointer hover:text-gray-700"></i
+                    ><i
+                      class="ri-login-box-line text-black cursor-pointer hover:text-gray-700"
+                    ></i
                   ></span>
-                  <p class="text-black pl-10 cursor-pointer hover:text-gray-700">
+                  <p
+                    class="text-black pl-10 cursor-pointer hover:text-gray-700"
+                  >
                     <RouterLink to="/login">Déjà inscrit ?</RouterLink>
                   </p>
                 </div>
@@ -173,9 +177,15 @@
                 <div class="flex flex-col gap-1">
                   <div class="relative">
                     <span class="icon-style"
-                      ><i class="ri-question-fill text-black cursor-pointer hover:text-gray-700"></i
+                      ><i
+                        class="ri-question-fill text-black cursor-pointer hover:text-gray-700"
+                      ></i
                     ></span>
-                    <p class="text-black pl-10 cursor-pointer hover:text-gray-700">Besoin d'aide ?</p>
+                    <p
+                      class="text-black pl-10 cursor-pointer hover:text-gray-700"
+                    >
+                      Besoin d'aide ?
+                    </p>
                   </div>
                 </div>
               </div>
@@ -188,68 +198,76 @@
 </template>
 <script setup>
 import { ref } from "vue";
-import axios from 'axios';
+import axios from "axios";
 import { RouterLink } from "vue-router";
-const utilisateur = ref(
-  {
-    firstname: "",
-    lastname: "",
-    email: "",
-    phone: "",
-    password:"",
-    password_confirmation:""
-  },
-);
-const erreurs = ref(
-  {
-    firstname: "",
-    email: "",
-    password:""
-  },
-);
+const utilisateur = ref({
+  firstname: "",
+  lastname: "",
+  email: "",
+  phone: "",
+  password: "",
+  password_confirmation: "",
+});
+const erreurs = ref({
+  firstname: "",
+  email: "",
+  password: "",
+});
 
 const soumettreFormulaire = async () => {
-  erreurs.value.firstname = utilisateur.value.firstname ? "" : "Le nom est obligatoire.";
-  erreurs.value.email = utilisateur.value.email ? "" : "L'email est obligatoire.";
-  erreurs.value.password = utilisateur.value.password? "" : "Le mot de passe est obligatoire";
+  erreurs.value.firstname = utilisateur.value.firstname
+    ? ""
+    : "Le nom est obligatoire.";
+  erreurs.value.email = utilisateur.value.email
+    ? ""
+    : "L'email est obligatoire.";
+  erreurs.value.password = utilisateur.value.password
+    ? ""
+    : "Le mot de passe est obligatoire";
 
   if (!utilisateur.value.firstname || !utilisateur.value.email) {
     return;
   }
 
   try {
-  const response = await axios.post("https://booking.openintech.app/api/register",
-    utilisateur.value, 
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "Shop-Id": "072f100a-9009-4e5c-98a2-007f2f24cf11",
-       
-      },
-    }
-  );
+    const response = await axios.post(
+      "https://booking.openintech.app/api/register",
+      utilisateur.value,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Shop-Id": "072f100a-9009-4e5c-98a2-007f2f24cf11",
+        },
+      }
+    );
 
-  console.log("Réponse de l'API :", response.data);
-  alert("Inscription réussie !");
-  localStorage.setItem('token', response.data.token); 
+    console.log("Réponse de l'API :", response.data);
+    alert("Inscription réussie !");
+    localStorage.setItem("token", response.data.token);
 
-
-  utilisateur.value = { firstname: "", lastname: "", email: "", phone: "", password:"", password_confirmation:"" };
-} catch (error) {
-  console.error("Erreur lors de l'inscription :", error);
-  alert(`Erreur : ${error.response?.data?.message || "Une erreur est survenue !"}`);
-}
-
+    utilisateur.value = {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: "",
+      password: "",
+      password_confirmation: "",
+    };
+  } catch (error) {
+    console.error("Erreur lors de l'inscription :", error);
+    alert(
+      `Erreur : ${error.response?.data?.message || "Une erreur est survenue !"}`
+    );
+  }
 };
-
 </script>
 <style scoped>
 .icon-style {
   position: absolute;
-  left: 10px;
+  left: 0.75rem; /* 12px */
   top: 50%;
   transform: translateY(-50%);
   color: #6b7280;
-  
+  z-index: 10;
 }
 </style>
